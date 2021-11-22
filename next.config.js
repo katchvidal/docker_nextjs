737f8c0195734ba01ffddd8a7110a76f21734439
Katch <katchvidal@gmail.com>
2021-11-22 17:22:41 -0600

    dockerized a nextjs

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
-module.exports = {
+/*module.exports = {
   reactStrictMode: true,
 }
+*/
+
+/*
+ * Parametros para que haga live Reaload con Docker
+ * Importante tenerlo si no hara Live Reload
+ *
+ */
+module.exports = {
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+
+    return config;
+  },
+};
